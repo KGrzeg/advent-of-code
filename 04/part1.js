3f77313f3fad8b12fed93f4831f0c3ba2c9e9de7
KGrzeg <kupczykgrzeg@gmail.com>
2021-12-05 03:21:10 +0100

    Day 04, a little gap

---
+++ 04/part1.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+
+const text = fs.readFileSync(process.stdin.fd, "utf8");
+const lines = text.split("\n");
+
+class Board {
+  cells = []
+  sum = 0
+  cols = [5, 5, 5, 5, 5];
+  rows = [5, 5, 5, 5, 5];
+
+  constructor(line_start) {
+    for (let i = line_start; i < line_start + 5; i++) {
+      this.cells.push(
+        lines[i]
+          .trim()
+          .split(/\s+/g)
+          .map(n => {
+            n = parseInt(n);
+            this.sum += n;
+            return n;
+          })
+      );
+    }
+  }
+
+  mark(number) {
+    for (let y = 0; y < 5; y++) {
+      for (let x = 0; x < 5; x++) {
+        if (this.cells[y][x] == number) {
+          this.sum -= number;
+          this.cols[x] -= 1;
+          this.rows[y] -= 1;
+        }
+      }
+    }
+
+    for (let i = 0; i < 5; i++)
+      if (this.rows[i] <= 0 || this.cols[i] <= 0)
+        return true;
+
+    return false;
+  }
+}
+
+
+const numbers = lines[0].split(",").map(n => parseInt(n));
+const boards = [];
+
+let line_counter = 2;
+while (line_counter < lines.length) {
+  boards.push(new Board(line_counter));
+  line_counter += 6;
+}
+
+for (const number of numbers) {
+  for (const board of boards) {
+    if (board.mark(number)) {
+      console.log(board.sum * number);
+      process.exit();
+    }
+  }
+}
+
+console.log("Answer not found? ;/");
